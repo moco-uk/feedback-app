2fb5cef7a8fc665ecb026041c0c09dd349199a24
monahvi <mehmet.moghaddam@gmail.com>
2023-04-16 20:38:51 +0100

    Dynamic values

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 function App() {
+    const tile = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id:1, text:'Comment one'},
+        {id:2, text:'Comment two'},
+        {id:3, text:'Comment three'},
+    ]
+
   return (
     <div className="container">
-      <h1>My app</h1>
+      <h1>{tile.toUpperCase()}</h1>
+      <p>{body}</p>
+
+    <div>
+        <h3>Comments({comments.length})</h3>
+        <ul>
+            {comments.map((comment, index) => (
+                <li key={index}>{comment.text}</li>
+            ))}
+        </ul>
+    </div>
+
     </div>
   );
 }
