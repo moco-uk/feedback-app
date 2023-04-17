94626054245a384e6de80beb90fa597ba0c53b89
monahvi <mehmet.moghaddam@gmail.com>
2023-04-17 20:33:15 +0100

    First componenet and props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
+import Header from "./components/Header";
+
 function App() {
-    const tile = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id:1, text:'Comment one'},
-        {id:2, text:'Comment two'},
-        {id:3, text:'Comment three'},
-    ]
-
   return (
+    <>
+    <Header />
+    
+
     <div className="container">
-      <h1>{tile.toUpperCase()}</h1>
-      <p>{body}</p>
-
-    <div>
-        <h3>Comments({comments.length})</h3>
-        <ul>
-            {comments.map((comment, index) => (
-                <li key={index}>{comment.text}</li>
-            ))}
-        </ul>
+      <h1>My app</h1>
     </div>
-
-    </div>
-  );
+    </>
+  )
 }
 
 export default App;
